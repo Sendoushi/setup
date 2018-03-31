92cc9792335d26866028e18b88ceb9892fb87e81
Joel Santos <joel@sendoushi.com>
2018-03-31 19:38:09 +0100

    [upd] Continue improvement...

--- sds_install.sh
+++ sds_install.sh
@@ -1,13 +1,22 @@
+echo "";
 echo "#######################################";
 echo "# SDS: Installing dependencies";
+echo "";
+
 sh $(pwd)/sds_install_dependencies.sh;
 
+echo "";
 echo "#######################################";
 echo "# SDS: Update";
+echo "";
+
 sh $(pwd)/sds_update.sh;
 
+echo "";
 echo "#######################################";
 echo "# SDS: Setting up";
+echo "";
+
 sh $(pwd)/sds_setting_up.sh;
 
 echo "" >> ~/.bashrc;
@@ -16,14 +25,22 @@ echo "if [ -f  ~/.userrc]; then" >> ~/.bashrc;
 echo "  . ~/.userrc;" >> ~/.bashrc;
 echo "fi" >> ~/.bashrc;
 
+echo "";
 echo "#######################################";
 echo "# SDS: Installing desktop environment";
+echo "";
+
 sh $(pwd)/sds_install_de.sh;
 
+echo "";
 echo "#######################################";
 echo "# SDS: Cleaning up";
+echo "";
+
 sudo apt-get clean;
 sudo apt-get autoremove;
 
+echo "";
 echo "#######################################";
 echo "# SDS: Setup all done";
+echo "";
