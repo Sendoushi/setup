af67cefee9ffe0bd12a60f49a98fa04a558ca568
Joel Santos <joel@sendoushi.com>
2018-03-31 19:45:11 +0100

    [upd] Continue improvement...

--- sds_install_dependencies.sh
+++ sds_install_dependencies.sh
@@ -1,7 +1,7 @@
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade;
 sudo apt-get install -y --fix-missing git htop cmake python-dev vim vim-nox curl tmux;
-curl -sL https://deb.nodesource.com/setup_8.x | sudo bash - ;
-sudo apt-get update;
-sudo apt-get install -y --fix-missing nodejs;
-rm -rf go1.10.linux-amd64.tar.gz && curl -O https://dl.google.com/go/go1.10.linux-amd64.tar.gz && tar xvf go1.10.linux-amd64.tar.gz && sudo rm -rf /usr/local/go && sudo mv go /usr/local && rm -rf go1.10.linux-amd64.tar.gz;
+rm -rf setup_* && curl -O https://deb.nodesource.com/setup_8.x && sudo sh setup_8.x && rm -rf setup_*;
+sudo apt-get update && sudo apt-get install -y --fix-missing nodejs;
+rm -rf go1* && curl -O https://dl.google.com/go/go1.10.linux-amd64.tar.gz && tar xvf go1.10.linux-amd64.tar.gz && sudo rm -rf /usr/local/go && sudo mv go /usr/local && rm -rf go1.10.linux-amd64.tar.gz;
 curl -sf -L https://static.rust-lang.org/rustup.sh | sh;
 sudo apt-get -y update && sudo apt-get -y upgrade;
