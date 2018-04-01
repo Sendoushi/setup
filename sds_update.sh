5f39130ea1a9c1ab1e32e272c5f9fe5b80826efc
Joel Santos <joel@sendoushi.com>
2018-04-01 16:54:03 +0100

    [upd] Vscode preferences and packages

--- sds_update.sh
+++ sds_update.sh
@@ -5,6 +5,7 @@ mkdir -p ~/work/src;
 rm -rf ~/go;
 ln -s ~/work ~/go;
 mkdir -p ~/.config;
+mkdir -p ~/.config/Code/User;
 
 rm -rf ~/.gitconfig;
 rm -rf ~/.gitignore_global;
@@ -18,6 +19,8 @@ rm -rf ~/.config/openbox;
 rm -rf ~/.config/polybar;
 rm -rf ~/.themes;
 rm -rf ~/.gtkrc-2.0;
+rm -rf ~/.config/Code/User;
+rm -rf ~/.vscode;
 sudo rm -rf /usr/lib/urxvt/perl/clipboard;
 
 cp .gitconfig ~/.gitconfig;
@@ -32,5 +35,11 @@ cp .gtkrc-2.0 ~/.gtkrc-2.0;
 sudo cp perlclipboard /usr/lib/urxvt/perl/clipboard;
 cp -rf openbox ~/.config/openbox;
 cp -rf polybar ~/.config/polybar;
+cp -rf Code/User ~/.config/Code/User;
+cp -rf .vscode ~/.vscode;
 
 git clone https://github.com/dglava/arc-openbox.git ~/.themes;
+
+git clone https://github.com/shlinux/faenza-icon-theme.git faenza;
+./faenza/INSTALL;
+rm -rf faenza;
