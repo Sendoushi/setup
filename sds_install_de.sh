09b4c710c9ef0423f03aff8ba1eb4388c0e42b63
Joel Santos <joel@sendoushi.com>
2018-04-01 12:26:33 +0100

    [upd] Ipmrove dependencies

--- sds_install_de.sh
+++ sds_install_de.sh
@@ -1,8 +1,12 @@
-sudo apt-get install -y --fix-missing dmenu rxvt-unicode xclip openbox obconf xinit build-essential pkg-config xorg openbox-themes lxappearance thunar;
+sudo apt-get install -y --fix-missing dmenu rxvt-unicode xclip openbox obconf xinit build-essential pkg-config xorg openbox-themes lxappearance thunar firefox;
 sudo update-alternatives --set x-terminal-emulator /usr/bin/urxvt;
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg;
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg;
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list';
+sudo apt-get install -y libxss1 libappindicator1 libindicator7;
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb;
+sudo dpkg -i google-chrome*;
+rm -rf google-chrome*;
+#curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg;
+#sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg;
+#sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list';
 sudo apt-get update;
 sudo apt-get install -y --fix-missing code;
 fc-cache -f -v;
