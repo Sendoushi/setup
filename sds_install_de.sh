14c7494ff9075e3dcbc0f91ec2804892650ad86a
Joel Santos <sendoushi@gmail.com>
2018-04-08 09:26:18 +0100

    [new] Automatically install sublime

--- sds_install_de.sh
+++ sds_install_de.sh
@@ -1,10 +1,8 @@
-sudo apt-get install -y --fix-missing dmenu rxvt-unicode xclip openbox obconf xinit build-essential pkg-config xorg lxappearance thunar firefox;
-sudo update-alternatives --set x-terminal-emulator /usr/bin/urxvt;
-sudo apt-get install -y libxss1 libappindicator1 libindicator7;
+# prepare
+rm -rf ~/.themes;
 
+# retrieve needed packages
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb;
-sudo dpkg -i google-chrome*;
-rm -rf google-chrome*;
 
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg;
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg;
@@ -12,20 +10,33 @@ sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode s
 sudo sh -c 'echo "\n\n# Custom code" >> /etc/apt/sources.list'; 
 sudo sh -c 'echo "\ndeb http://archive.getdeb.net/ubuntu zesty-getdeb apps" >> /etc/apt/sources.list'; 
 wget -q -O- http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add - ;
-sudo apt-get update;
-sudo apt-get install -y --fix-broken;
-sudo apt-get install -y --fix-missing code polybar;
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -;
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
 wget https://noto-website-2.storage.googleapis.com/pkgs/Noto-hinted.zip;
+
+# finally install all
+sudo apt-get update;
+
+sudo apt-get install -y --fix-missing dmenu rxvt-unicode xclip openbox obconf xinit build-essential pkg-config xorg lxappearance thunar firefox libxss1 libappindicator1 libindicator7 sublime-text code polybar;
+sudo update-alternatives --set x-terminal-emulator /usr/bin/urxvt;;
+sudo dpkg -i google-chrome*;
+
+sudo apt-get install -y --fix-broken;
+
 sudo install -d -m755 /usr/share/fonts/noto;
 sudo unzip Noto-hinted.zip -d /usr/share/fonts/noto;
 sudo chmod 0644 /usr/share/fonts/noto/*;
-rm -rf Noto*;
-fc-cache -f -v;
 
-rm -rf ~/.themes;
 git clone https://github.com/dglava/arc-openbox.git ~/.themes;
-
 git clone https://github.com/shlinux/faenza-icon-theme.git faenza;
+
+# setup
+fc-cache -f -v;
 ./faenza/INSTALL;
+
+# cleanup
+rm -rf Noto*;
+rm -rf google-chrome*;
 rm -rf faenza;
