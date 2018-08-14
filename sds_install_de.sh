595b0c945aaf2491408d7e38a729170e1a8aad4b
Joel Santos <joel@sendoushi.com>
2018-08-14 20:49:13 +0100

    [upd] Improve more

--- sds_install_de.sh
+++ sds_install_de.sh
@@ -8,12 +8,12 @@ sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg;
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list';
 
 sudo add-apt-repository ppa:jtaylor/keepass;
-
+sudo add-apt-repository ppa:daniruiz/flat-remix;
 
 # finally install all
 sudo apt-get update;
 
-sudo apt-get install -y --fix-missing xclip build-essential pkg-config firefox code apt-transport-https git-gui xdotool keepass2 ;
+sudo apt-get install -y --fix-missing xclip build-essential pkg-config firefox code apt-transport-https git-gui xdotool keepass2  arc-theme flat-remix-gtk;
 sudo dpkg -i google-chrome*;
 
 
